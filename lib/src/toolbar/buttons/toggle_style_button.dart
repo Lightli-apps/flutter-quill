743acc7c9cec61f92995d0cb297e98dcff27f57a
Tea-D <tea.devcic@cinnamon.agency>
2024-10-01 13:19:20 +0200

    Set left alignment as default

--- lib/src/toolbar/buttons/toggle_style_button.dart
+++ lib/src/toolbar/buttons/toggle_style_button.dart
@@ -11,15 +11,15 @@ import '../simple_toolbar_provider.dart';
 import '../theme/quill_icon_theme.dart';
 
 typedef ToggleStyleButtonBuilder = Widget Function(
-    BuildContext context,
-    Attribute attribute,
-    IconData icon,
-    bool? isToggled,
-    VoidCallback? onPressed,
-    VoidCallback? afterPressed, [
-    double iconSize,
-    QuillIconTheme? iconTheme,
-    ]);
+  BuildContext context,
+  Attribute attribute,
+  IconData icon,
+  bool? isToggled,
+  VoidCallback? onPressed,
+  VoidCallback? afterPressed, [
+  double iconSize,
+  QuillIconTheme? iconTheme,
+]);
 
 class QuillToolbarToggleStyleButton extends QuillToolbarToggleStyleBaseButton {
   const QuillToolbarToggleStyleButton({
@@ -32,12 +32,10 @@ class QuillToolbarToggleStyleButton extends QuillToolbarToggleStyleBaseButton {
   final Attribute attribute;
 
   @override
-  QuillToolbarToggleStyleButtonState createState() =>
-      QuillToolbarToggleStyleButtonState();
+  QuillToolbarToggleStyleButtonState createState() => QuillToolbarToggleStyleButtonState();
 }
 
-class QuillToolbarToggleStyleButtonState extends QuillToolbarToggleStyleBaseButtonState<
-    QuillToolbarToggleStyleButton> {
+class QuillToolbarToggleStyleButtonState extends QuillToolbarToggleStyleBaseButtonState<QuillToolbarToggleStyleButton> {
   Style get _selectionStyle => controller.getSelectionStyle();
 
   @override
@@ -46,173 +44,189 @@ class QuillToolbarToggleStyleButtonState extends QuillToolbarToggleStyleBaseButt
   (String, Widget) get _defaultTooltipAndIconData {
     switch (widget.attribute.key) {
       case 'bold':
-        return (context.loc.bold, SvgPicture.asset(
-          'assets/icons/text_style_toolbar/bold_icon.svg', fit: BoxFit.scaleDown,
-          colorFilter: ColorFilter.mode(currentStateValue ?
-          Theme
-              .of(context)
-              .brightness == Brightness.light ?
-          Colors.white :
-          Colors.black : Theme
-              .of(context)
-              .textTheme
-              .headlineSmall!
-              .color!, BlendMode.srcIn),
-        ));
+        return (
+          context.loc.bold,
+          SvgPicture.asset(
+            'assets/icons/text_style_toolbar/bold_icon.svg',
+            fit: BoxFit.scaleDown,
+            colorFilter: ColorFilter.mode(
+                currentStateValue
+                    ? Theme.of(context).brightness == Brightness.light
+                        ? Colors.white
+                        : Colors.black
+                    : Theme.of(context).textTheme.headlineSmall!.color!,
+                BlendMode.srcIn),
+          )
+        );
       case 'script':
         if (widget.attribute.value == ScriptAttributes.sub.value) {
           return (context.loc.subscript, const Icon(Icons.subscript));
         }
         return (context.loc.superscript, const Icon(Icons.superscript));
       case 'italic':
-        return (context.loc.italic, SvgPicture.asset(
-          'assets/icons/text_style_toolbar/italic_icon.svg', fit: BoxFit.scaleDown,
-          colorFilter: ColorFilter.mode(currentStateValue ?
-          Theme
-              .of(context)
-              .brightness == Brightness.light ?
-          Colors.white :
-          Colors.black : Theme
-              .of(context)
-              .textTheme
-              .headlineSmall!
-              .color!, BlendMode.srcIn),));
+        return (
+          context.loc.italic,
+          SvgPicture.asset(
+            'assets/icons/text_style_toolbar/italic_icon.svg',
+            fit: BoxFit.scaleDown,
+            colorFilter: ColorFilter.mode(
+                currentStateValue
+                    ? Theme.of(context).brightness == Brightness.light
+                        ? Colors.white
+                        : Colors.black
+                    : Theme.of(context).textTheme.headlineSmall!.color!,
+                BlendMode.srcIn),
+          )
+        );
       case 'small':
         return (context.loc.small, const Icon(Icons.format_size));
       case 'underline':
-        return (context.loc.underline, SvgPicture.asset(
-          'assets/icons/text_style_toolbar/underline_icon.svg', fit: BoxFit.scaleDown,
-          colorFilter: ColorFilter.mode(currentStateValue ?
-          Theme
-              .of(context)
-              .brightness == Brightness.light ?
-          Colors.white :
-          Colors.black : Theme
-              .of(context)
-              .textTheme
-              .headlineSmall!
-              .color!, BlendMode.srcIn),));
+        return (
+          context.loc.underline,
+          SvgPicture.asset(
+            'assets/icons/text_style_toolbar/underline_icon.svg',
+            fit: BoxFit.scaleDown,
+            colorFilter: ColorFilter.mode(
+                currentStateValue
+                    ? Theme.of(context).brightness == Brightness.light
+                        ? Colors.white
+                        : Colors.black
+                    : Theme.of(context).textTheme.headlineSmall!.color!,
+                BlendMode.srcIn),
+          )
+        );
       case 'strike':
-        return (context.loc.strikeThrough, SvgPicture.asset(
-          'assets/icons/text_style_toolbar/strikethrough_icon.svg', fit: BoxFit.scaleDown,
-          colorFilter: ColorFilter.mode(currentStateValue ?
-          Theme
-              .of(context)
-              .brightness == Brightness.light ?
-          Colors.white :
-          Colors.black : Theme
-              .of(context)
-              .textTheme
-              .headlineSmall!
-              .color!, BlendMode.srcIn),));
+        return (
+          context.loc.strikeThrough,
+          SvgPicture.asset(
+            'assets/icons/text_style_toolbar/strikethrough_icon.svg',
+            fit: BoxFit.scaleDown,
+            colorFilter: ColorFilter.mode(
+                currentStateValue
+                    ? Theme.of(context).brightness == Brightness.light
+                        ? Colors.white
+                        : Colors.black
+                    : Theme.of(context).textTheme.headlineSmall!.color!,
+                BlendMode.srcIn),
+          )
+        );
       case 'code':
         return (context.loc.inlineCode, const Icon(Icons.code));
       case 'direction':
         return (context.loc.textDirection, const Icon(Icons.format_textdirection_r_to_l));
       case 'list':
         if (widget.attribute.value == 'bullet') {
-          return (context.loc.bulletList, SvgPicture.asset(
-            'assets/icons/text_style_toolbar/bullet_paragraph.svg', fit: BoxFit.scaleDown,
-            colorFilter: ColorFilter.mode(currentStateValue ?
-            Theme
-                .of(context)
-                .brightness == Brightness.light ?
-            Colors.white :
-            Colors.black : Theme
-                .of(context)
-                .textTheme
-                .headlineSmall!
-                .color!, BlendMode.srcIn),));
+          return (
+            context.loc.bulletList,
+            SvgPicture.asset(
+              'assets/icons/text_style_toolbar/bullet_paragraph.svg',
+              fit: BoxFit.scaleDown,
+              colorFilter: ColorFilter.mode(
+                  currentStateValue
+                      ? Theme.of(context).brightness == Brightness.light
+                          ? Colors.white
+                          : Colors.black
+                      : Theme.of(context).textTheme.headlineSmall!.color!,
+                  BlendMode.srcIn),
+            )
+          );
         }
-        return (context.loc.numberedList, SvgPicture.asset(
-          'assets/icons/text_style_toolbar/number_paragraph.svg', fit: BoxFit.scaleDown,
-          colorFilter: ColorFilter.mode(currentStateValue ?
-          Theme
-              .of(context)
-              .brightness == Brightness.light ?
-          Colors.white :
-          Colors.black : Theme
-              .of(context)
-              .textTheme
-              .headlineSmall!
-              .color!, BlendMode.srcIn),));
+        return (
+          context.loc.numberedList,
+          SvgPicture.asset(
+            'assets/icons/text_style_toolbar/number_paragraph.svg',
+            fit: BoxFit.scaleDown,
+            colorFilter: ColorFilter.mode(
+                currentStateValue
+                    ? Theme.of(context).brightness == Brightness.light
+                        ? Colors.white
+                        : Colors.black
+                    : Theme.of(context).textTheme.headlineSmall!.color!,
+                BlendMode.srcIn),
+          )
+        );
       case 'code-block':
         return (context.loc.codeBlock, const Icon(Icons.code));
       case 'blockquote':
         return (context.loc.quote, const Icon(Icons.format_quote));
       case 'align':
         return switch (widget.attribute.value) {
-          'left' =>
-          (context.loc.alignLeft, SvgPicture.asset(
-            'assets/icons/text_style_toolbar/left_alignment.svg', fit: BoxFit.scaleDown,
-            colorFilter: ColorFilter.mode(currentStateValue ?
-            Theme
-                .of(context)
-                .brightness == Brightness.light ?
-            Colors.white :
-            Colors.black : Theme
-                .of(context)
-                .textTheme
-                .headlineSmall!
-                .color!, BlendMode.srcIn),)),
-          'right' =>
-          (context.loc.alignRight, SvgPicture.asset(
-            'assets/icons/text_style_toolbar/right_alignment.svg', fit: BoxFit.scaleDown,
-            colorFilter: ColorFilter.mode(currentStateValue ?
-            Theme
-                .of(context)
-                .brightness == Brightness.light ?
-            Colors.white :
-            Colors.black : Theme
-                .of(context)
-                .textTheme
-                .headlineSmall!
-                .color!, BlendMode.srcIn),)),
-          'center' =>
-          (context.loc.alignCenter, SvgPicture.asset(
-            'assets/icons/text_style_toolbar/center_alignment.svg', fit: BoxFit.scaleDown,
-            colorFilter: ColorFilter.mode(currentStateValue ?
-            Theme
-                .of(context)
-                .brightness == Brightness.light ?
-            Colors.white :
-            Colors.black : Theme
-                .of(context)
-                .textTheme
-                .headlineSmall!
-                .color!, BlendMode.srcIn),)),
-          'justify' =>
-          (context.loc.alignJustify, SvgPicture.asset(
-            'assets/icons/text_style_toolbar/justified_alignment.svg', fit: BoxFit.scaleDown,
-            colorFilter: ColorFilter.mode(currentStateValue ?
-            Theme
-                .of(context)
-                .brightness == Brightness.light ?
-            Colors.white :
-            Colors.black : Theme
-                .of(context)
-                .textTheme
-                .headlineSmall!
-                .color!, BlendMode.srcIn),)),
+          'left' => (
+              context.loc.alignLeft,
+              SvgPicture.asset(
+                'assets/icons/text_style_toolbar/left_alignment.svg',
+                fit: BoxFit.scaleDown,
+                colorFilter: ColorFilter.mode(
+                    currentStateValue
+                        ? Theme.of(context).brightness == Brightness.light
+                            ? Colors.white
+                            : Colors.black
+                        : Theme.of(context).textTheme.headlineSmall!.color!,
+                    BlendMode.srcIn),
+              )
+            ),
+          'right' => (
+              context.loc.alignRight,
+              SvgPicture.asset(
+                'assets/icons/text_style_toolbar/right_alignment.svg',
+                fit: BoxFit.scaleDown,
+                colorFilter: ColorFilter.mode(
+                    currentStateValue
+                        ? Theme.of(context).brightness == Brightness.light
+                            ? Colors.white
+                            : Colors.black
+                        : Theme.of(context).textTheme.headlineSmall!.color!,
+                    BlendMode.srcIn),
+              )
+            ),
+          'center' => (
+              context.loc.alignCenter,
+              SvgPicture.asset(
+                'assets/icons/text_style_toolbar/center_alignment.svg',
+                fit: BoxFit.scaleDown,
+                colorFilter: ColorFilter.mode(
+                    currentStateValue
+                        ? Theme.of(context).brightness == Brightness.light
+                            ? Colors.white
+                            : Colors.black
+                        : Theme.of(context).textTheme.headlineSmall!.color!,
+                    BlendMode.srcIn),
+              )
+            ),
+          'justify' => (
+              context.loc.alignJustify,
+              SvgPicture.asset(
+                'assets/icons/text_style_toolbar/justified_alignment.svg',
+                fit: BoxFit.scaleDown,
+                colorFilter: ColorFilter.mode(
+                    currentStateValue
+                        ? Theme.of(context).brightness == Brightness.light
+                            ? Colors.white
+                            : Colors.black
+                        : Theme.of(context).textTheme.headlineSmall!.color!,
+                    BlendMode.srcIn),
+              )
+            ),
           Object() => throw ArgumentError(widget.attribute.value),
-          null =>
-          (context.loc.alignLeft, SvgPicture.asset(
-            'assets/icons/text_style_toolbar/left_alignment.svg', fit: BoxFit.scaleDown,
-            colorFilter: ColorFilter.mode(widget.attribute.value == null ?
-            Theme
-                .of(context)
-                .brightness == Brightness.light ?
-            Colors.white :
-            Colors.black : Theme
-                .of(context)
-                .textTheme
-                .headlineSmall!
-                .color!, BlendMode.srcIn),), ),
+          null => (
+              context.loc.alignLeft,
+              SvgPicture.asset(
+                'assets/icons/text_style_toolbar/left_alignment.svg',
+                fit: BoxFit.scaleDown,
+                colorFilter: ColorFilter.mode(
+                    !_getIsToggled({Attribute.align.key: Attribute.color})
+                        ? Theme.of(context).brightness == Brightness.light
+                            ? Colors.white
+                            : Colors.black
+                        : Theme.of(context).textTheme.headlineSmall!.color!,
+                    BlendMode.srcIn),
+              ),
+            ),
         };
       default:
         throw ArgumentError(
           'Could not find the default tooltip for '
-              '${widget.attribute.toString()}',
+          '${widget.attribute.toString()}',
         );
     }
   }
@@ -230,8 +244,7 @@ class QuillToolbarToggleStyleButtonState extends QuillToolbarToggleStyleBaseButt
 
   @override
   Widget build(BuildContext context) {
-    final childBuilder = options.childBuilder ??
-        context.quillToolbarBaseButtonOptions?.childBuilder;
+    final childBuilder = options.childBuilder ?? context.quillToolbarBaseButtonOptions?.childBuilder;
     if (childBuilder != null) {
       return childBuilder(
         options,
@@ -277,23 +290,22 @@ class QuillToolbarToggleStyleButtonState extends QuillToolbarToggleStyleBaseButt
     controller
       ..skipRequestKeyboard = !widget.attribute.isInline
       ..formatSelection(
-        currentValue
-            ? Attribute.clone(widget.attribute, null)
-            : widget.attribute,
+        currentValue ? Attribute.clone(widget.attribute, null) : widget.attribute,
       );
   }
 }
 
-Widget defaultToggleStyleButtonBuilder(BuildContext context,
-    Attribute attribute,
-    Widget icon,
-    bool? isToggled,
-    VoidCallback? onPressed,
-    VoidCallback? afterPressed, [
-      double iconSize = kDefaultIconSize,
-      double iconButtonFactor = kDefaultIconButtonFactor,
-      QuillIconTheme? iconTheme,
-    ]) {
+Widget defaultToggleStyleButtonBuilder(
+  BuildContext context,
+  Attribute attribute,
+  Widget icon,
+  bool? isToggled,
+  VoidCallback? onPressed,
+  VoidCallback? afterPressed, [
+  double iconSize = kDefaultIconSize,
+  double iconButtonFactor = kDefaultIconButtonFactor,
+  QuillIconTheme? iconTheme,
+]) {
   final isEnabled = onPressed != null;
   return QuillToolbarIconButton(
     icon: icon,
