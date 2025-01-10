c78c2c04c4e0d9b90c5caffa372aba534aff62ae
Tea-D <tea.devcic@cinnamon.agency>
2025-01-10 11:38:09 +0100

    Add ink instead of container

--- lib/src/toolbar/buttons/quill_icon_button.dart
+++ lib/src/toolbar/buttons/quill_icon_button.dart
@@ -1,8 +1,6 @@
-import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../theme/quill_icon_theme.dart';
 
 class QuillToolbarIconButton extends StatelessWidget {
@@ -28,14 +26,14 @@ class QuillToolbarIconButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      splashColor: Theme.of(context).brightness == Brightness.light ? const Color(0xFF474747) : const Color(0xFFF6F6F6),
+      splashColor: const Color(0xFFCCCCCC),
       onTap: onPressed != null
           ? () {
               onPressed?.call();
               afterPressed?.call();
             }
           : null,
-      child: Container(
+      child: Ink(
         width: 30.w,
         height: 30.h,
         decoration: BoxDecoration(
