64bc655cd14cb69c2743fda141ae7b52452cc088
Tea-D <tea.devcic@cinnamon.agency>
2025-01-10 14:16:34 +0100

    Font size - inkwell

--- lib/src/widgets/textstyle_buttons_quill.dart
+++ lib/src/widgets/textstyle_buttons_quill.dart
@@ -23,46 +23,51 @@ class TextstyleButtonQuill extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: width.w),
-      child: GestureDetector(
-        onTap: onTap,
-        child: Container(
-          width: 30.w,
-          height: 30.h,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(
-              Radius.circular(20.r),
+      child: Material(
+        borderRadius: BorderRadius.all(Radius.circular(20.r)),
+        child: InkWell(
+          borderRadius: BorderRadius.all(Radius.circular(20.r)),
+          splashColor: const Color(0xFFCCCCCC),
+          onTap: onTap,
+          child: Ink(
+            width: 30.w,
+            height: 30.h,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(
+                Radius.circular(20.r),
+              ),
+              color: isOn
+                  ? Theme.of(context).brightness == Brightness.light
+                      ? const Color(0xFF474747)
+                      : const Color(0xFFF6F6F6)
+                  : Colors.transparent,
             ),
-            color: isOn
-                ? Theme.of(context).brightness == Brightness.light
-                    ? const Color(0xFF474747)
-                    : const Color(0xFFF6F6F6)
-                : Colors.transparent,
-          ),
-          child: noIcon
-              ? Center(
-                  child: Text(
-                    icon,
-                    style: isOn
-                        ? Theme.of(context).textTheme.bodySmall?.copyWith(
-                            color: Theme.of(context).brightness == Brightness.light
-                                ? Colors.white
-                                : const Color(0xFF3a3a3a))
-                        : Theme.of(context).textTheme.bodySmall,
-                  ),
-                )
-              : SvgPicture.asset(
-                  icon,
-                  fit: BoxFit.scaleDown,
-                  colorFilter: isColorPicker
-                      ? null
-                      : ColorFilter.mode(
-                          isOn
-                              ? Theme.of(context).brightness == Brightness.light
+            child: noIcon
+                ? Center(
+                    child: Text(
+                      icon,
+                      style: isOn
+                          ? Theme.of(context).textTheme.bodySmall?.copyWith(
+                              color: Theme.of(context).brightness == Brightness.light
                                   ? Colors.white
-                                  : const Color(0xFF3a3a3a)
-                              : Theme.of(context).textTheme.headlineSmall!.color!,
-                          BlendMode.srcIn),
-                ),
+                                  : const Color(0xFF3a3a3a))
+                          : Theme.of(context).textTheme.bodySmall,
+                    ),
+                  )
+                : SvgPicture.asset(
+                    icon,
+                    fit: BoxFit.scaleDown,
+                    colorFilter: isColorPicker
+                        ? null
+                        : ColorFilter.mode(
+                            isOn
+                                ? Theme.of(context).brightness == Brightness.light
+                                    ? Colors.white
+                                    : const Color(0xFF3a3a3a)
+                                : Theme.of(context).textTheme.headlineSmall!.color!,
+                            BlendMode.srcIn),
+                  ),
+          ),
         ),
       ),
     );
