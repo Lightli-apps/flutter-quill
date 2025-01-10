9412489b84a54bbfbf9f9034e29dfaaa371bf569
Tea-D <tea.devcic@cinnamon.agency>
2025-01-10 13:02:19 +0100

    Add material widget

--- lib/src/toolbar/buttons/quill_icon_button.dart
+++ lib/src/toolbar/buttons/quill_icon_button.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../theme/quill_icon_theme.dart';
@@ -25,28 +24,34 @@ class QuillToolbarIconButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      splashColor: const Color(0xFFCCCCCC),
-      onTap: onPressed != null
-          ? () {
-              onPressed?.call();
-              afterPressed?.call();
-            }
-          : null,
-      child: Ink(
-        width: 30.w,
-        height: 30.h,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(
-            Radius.circular(20.r),
+    return Material(
+      borderRadius: BorderRadius.all(
+        Radius.circular(20.r),
+      ),
+      color: Colors.transparent,
+      child: InkWell(
+        splashColor: const Color(0xFFCCCCCC),
+        onTap: onPressed != null
+            ? () {
+                onPressed?.call();
+                afterPressed?.call();
+              }
+            : null,
+        child: Ink(
+          width: 30.w,
+          height: 30.h,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(
+              Radius.circular(20.r),
+            ),
+            color: isSelected
+                ? Theme.of(context).brightness == Brightness.light
+                    ? const Color(0xFF474747)
+                    : const Color(0xFFF6F6F6)
+                : Colors.transparent,
           ),
-          color: isSelected
-              ? Theme.of(context).brightness == Brightness.light
-                  ? const Color(0xFF474747)
-                  : const Color(0xFFF6F6F6)
-              : Colors.transparent,
+          child: icon,
         ),
-        child: icon,
       ),
     );
   }
